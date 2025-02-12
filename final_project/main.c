f9aef1ca405f49a72be4849be4f4e5f14fb7efb0
Quater <102786860+quanganh296@users.noreply.github.com>
2025-02-12 13:14:17 +0700

    Update main.c

--- final_project/main.c
+++ final_project/main.c
@@ -3,15 +3,7 @@
 #include <string.h>
 #include "datatype.h"
 
-//void loadUsersFromFile() {
-//    // Implementation of loading users from file
-//    printf("Loading users from file...\n");
-//}
-//
-//void displayRoleMenu() {
-//    // Implementation of displaying role menu
-//    printf("Displaying role menu...\n");
-//}
+
 int main() {
     loadUsersFromFile();
     displayRoleMenu();
